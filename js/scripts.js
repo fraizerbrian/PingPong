7482c72cb94e8eed589fe10086080cec008ab86f
fraizer <fraizerkilonzo@gmail.com>
2018-07-13 11:13:13 +0300

    added background image

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#play").click(funtion(event){
+        event.preventDefault();
+        var #Input1= parseInt($("#input1").val());
+        if ((userInput %5)===0){
+            document.write ["Ping"];
+        };
+        elseIf ((userInput %3)===0){
+            document.write ["Pong"];
+        };
+        elseIf ((userInput %5) $$ (userInput%3)===0){
+            document.write ["Ping-Pong"];
+        };
+        elseIf ({document.write ['Input1']});
+    });
+});
